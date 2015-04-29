68ec2d133f5ae2b3958fd21d2781980f5a76550f
Benson Li <lzbin90@gmail.com>
2015-04-28 20:40:26 -0400

    finish: negation handler

--- src/raw_data_processing/NegationHandler.java
+++ src/raw_data_processing/NegationHandler.java
@@ -1,17 +1,16 @@
 package raw_data_processing;
 
 import opennlp.tools.postag.POSModel;
-import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by
@@ -20,61 +19,153 @@ import java.util.List;
  * Email: zl791@nyu.edu
  */
 public class NegationHandler {
+    public HashSet<String> negation_words;
+    public List<String[]> splitSentences;
+    public HashSet<String> tagsToBeModified;
+    public List<String[]> resultSentences;
+    public String inputFileName;
+    public String inputFilePath;
+    public String outputFilePath;
+    public String outputFileName;
 
-    public void handleNegation() throws IOException {
-        String text = new String(Files.readAllBytes(Paths.get("src/raw_data_processing/data/testFile")));
-        text = text.replace("\n", "").replace("\r", "");
 
+    public NegationHandler() {
+        splitSentences = new ArrayList<String[]>();
+        negation_words = new HashSet<String>();
+        tagsToBeModified = new HashSet<String>();
+        resultSentences = new ArrayList<String[]>();
+        tagsToBeModified.add("RB");
+        tagsToBeModified.add("JJR");
+        tagsToBeModified.add("RBR");
+        tagsToBeModified.add("JJ");
+        tagsToBeModified.add("JJS");
+        tagsToBeModified.add("RBS");
+        tagsToBeModified.add("RB");
+        tagsToBeModified.add("VB");
+        tagsToBeModified.add("VBD");
+        tagsToBeModified.add("VBG");
+        tagsToBeModified.add("VBN");
+        tagsToBeModified.add("VBP");
+        tagsToBeModified.add("VBZ");
+    }
 
-        String[] splitSencence = SentenceDetection.splitSentence("src/model/en-sent.bin", text);
-        for(String sentence:splitSencence){
-            System.out.println(sentence);
+    public void addSplitSentence(File file) throws IOException {
+        String line;
+        InputStream fis = new FileInputStream(file);
+        String absolutePath = file.getAbsolutePath();
+        inputFilePath = absolutePath.
+                substring(0,absolutePath.lastIndexOf(File.separator));
+        inputFileName= file.getName();
+
+        InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+        BufferedReader br = new BufferedReader(isr);
+        while ((line = br.readLine()) != null) {
+            line = line.replace("\n", "");
+            String[] splitSent = SentenceDetection.splitSentence("src/model/en-sent.bin", line);
+            splitSentences.add(splitSent);
+
         }
+    }
 
-        HashSet<String> negation_words = new HashSet<String>();
-        List<String> lines=Files.readAllLines(Paths.get("src/raw_data_processing/data/negation_word_list"), Charset.forName("UTF-8"));
+    public void addNegationWords(String file) throws IOException {
+        List<String> lines=Files.readAllLines(Paths.get(file), Charset.forName("UTF-8"));
+        if(lines.isEmpty()) throw new NoSuchElementException("negation lists empty");
         for(String line:lines){
-            negation_words.add(line);
+            negation_words.add(line.toLowerCase());
         }
+    }
 
-        InputStream modelIn = null;
-        modelIn = new FileInputStream("src/model/en-pos-maxent.bin");
+    public void handleAndSaveToFile(String path, String name) throws IOException {
+        InputStream modelIn = new FileInputStream("src/model/en-pos-maxent.bin");
         POSModel model = new POSModel(modelIn);
         POSTaggerME tagger = new POSTaggerME(model);
 
-        for(String sentence:splitSencence){
-            String[] sent = sentence.split(" ");
-            String[] tags = tagger.tag(sent);
-            double probs[] = tagger.probs();
-            for(String tag: tags) {
-                System.out.println(tag);
-            }
-            POSSample sample = new POSSample(sent, tags);
-            for(String s : negation_words) {
-                System.out.println("-"+s);
-            }
+        if(splitSentences == null) throw new NullPointerException("splitSentence is null");
+        for(String[] splitSent : splitSentences){
+            List<String> modifiedLine = new ArrayList<String>();
+            for(String sentence:splitSent){
+                String[] words = sentence.split(" ");
+                String[] tags = tagger.tag(words);
+                double probs[] = tagger.probs();
 
-            for( int i = 0; i < tags.length; i++){
-                String word = sent[i];
-                if(negation_words.contains(word)){
-                    System.out.println("========="+word);
-                    int endIndex = i;
-                    for(endIndex = i; endIndex < tags.length; endIndex++){
-                        if(tags[endIndex].equals("CC")){
-                            System.out.println("------"+tags[endIndex]);
-                            break;
+
+                // one sentence in a line
+                for( int i = 1; i < tags.length; i++){
+                    String curWord = words[i];
+                    if(negation_words.contains(curWord.toLowerCase())){
+                        int endIndex;
+                        for(endIndex = i; endIndex < tags.length; endIndex++){
+                            if(tags[endIndex].equals("CC") || tags[endIndex].equals(",") || tags[endIndex].equals(";")){
+                                break;
+                            }
+                        }
+                        for(int j = i+1; j < endIndex; j++){
+                            if(tagsToBeModified.contains(tags[j])){
+                                words[j] = "NOT_"+words[j];
+                            }
                         }
                     }
-                    for(int j = i; j < endIndex; j++){
-                        sent[j] = "NOT_"+sent[j];
-                    }
-
                 }
-            }
+//                POSSample sample = new POSSample(words, tags);
+//                System.out.println(sample.toString());
 
-            for(String word: sent){
-                System.out.println(word);
+                modifiedLine.add(makeSentence(words));
+
+            }
+            String[] sentArray = new String[modifiedLine.size()];
+            sentArray = modifiedLine.toArray(sentArray);
+            resultSentences.add(sentArray);
+
+
+        }
+
+        Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path+"/"+name),"utf-8"));
+        for(String[] sent : resultSentences){
+            for(String s : sent){
+//                System.out.print(s);
+                writer.write(s);
+            }
+            writer.write("\n");
+//            System.out.println();
+        }
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        NegationHandler negationHandler = new NegationHandler();
+//        File folder = new File("src/raw_data_processing/data/review");
+        File folder = new File("data/reviews");
+
+        File[] listOfFiles = folder.listFiles();
+
+
+        System.out.println("task begin");
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                negationHandler.addSplitSentence(listOfFiles[i]);
+                negationHandler.addNegationWords("src/raw_data_processing/data/negation_word_list");
+                String outputPath = negationHandler.inputFilePath+"_after_negation";
+                negationHandler.handleAndSaveToFile(outputPath,negationHandler.inputFileName);
+                System.out.println("#"+i+": "+negationHandler.inputFileName+" - done");
+
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
             }
         }
+        System.out.println("all done");
     }
+
+    public String makeSentence(String[] words) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < words.length; i++){
+            sb.append(words[i]);
+            if(i != words.length - 1){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
 }
